a57cc39998a82f9a46cb404828d410069c7a8c2c
Alper Canimoglu <canimoglu185@gmail.com>
2022-10-25 17:12:35 +0300

    Initial Commit

---
+++ KennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KennyGame
+//
+//  Created by Alper Canımoğlu on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
